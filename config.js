59958da5981ab6586d91423b3e6dd022d38e9043
Nam vu <namdeppz@gmmail.com>
2024-10-03 21:23:22 +0700

    frist commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Hế lu",
+    introTitle: 'Anh là chàng trai có trái tim màu đỏ\nVừa đủ nhỏ để chứa một mình em',
+    introDesc: 'Anh muốn hỏi em câu này. Em trả lời nha',
+    btnIntro: '^^Oke HiHi^^',
+    title: 'Anh đây thì vẫn một mình nếu em cũng thế thì mình iu nhau 🥰',
+    desc: 'Thoát ra là chấp nhận yêu Anh rồi nhé!!!',
+    btnYes: 'Em đồng ý <33',
+    btnNo: 'Không nha :3',
+    question: 'Trời xanh mây trắng nắng vàng.\nHỏi Hà nay đã sẵn sàng yêu chưa?',
+    btnReply: 'Gửi lời iu thưn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'ANh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Nhắn anh thời gian đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://facebook.com/vpn2101'
+}
